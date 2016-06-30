5e873acdbb74b99ffdd0b8238af8c95e2ef379b3
Zack Mitchell <zackcmitchell@gmail.com>
2016-06-30 14:32:41 -0400

    Styled Search template

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,22 +9,57 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+<div class="blog-roll-item">
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php juno_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+    <article data-link="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>" id="post-<?php esc_attr( the_ID() ); ?>" <?php post_class(); ?>>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+        <?php if ( has_post_thumbnail() ) : ?>
+            <div class="image">
+                <a href="<?php echo esc_url( get_the_permalink() ); ?>">
+                    <?php esc_html( the_post_thumbnail( 'medium' ) ); ?>
+                </a>
+            </div>
+        <?php endif; ?>
+        
+        <div class="inner">
 
-	<footer class="entry-footer">
-		<?php juno_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+            <h6 class="post-category">
+                <?php 
+                    $categories = get_the_category();
+                    if ( ! empty( $categories ) ) {
+                        echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                    }
+                ?>
+            </h6>
+            
+            <h3 class="post-title">
+                <a href="<?php echo esc_url( get_the_permalink() ); ?>">
+                    <?php esc_html( the_title() ); ?>
+                </a>
+            </h3>
+            
+            <hr>
+            
+            <div class="post-content">
+                <?php echo wp_trim_words( strip_tags( get_the_content() ), 50 ); ?>
+            </div>
+
+            <?php if ( get_theme_mod( 'juno_blog_show_date', 'show' ) == 'show' || get_theme_mod( 'juno_blog_show_author', 'show' ) == 'show' ) : ?>
+                <h5 class="post-meta">
+                    <?php echo get_theme_mod( 'juno_blog_show_date', 'show' ) == 'show' ? juno_posted_on() : ''; ?>
+                    <?php if ( get_theme_mod( 'juno_blog_show_author', 'show' ) == 'show' ) : ?>    
+                    by <span class="post-author"><?php esc_html( the_author_posts_link() ); ?></span>
+                    <?php endif; ?>
+                </h5>
+            <?php endif; ?>
+            
+            <div class="image-corner" style="background-image: url(<?php echo esc_url( get_template_directory_uri() . '/inc/images/blog-corner-skin-1.png' ); ?>);"></div>
+            <a href="<?php the_permalink() ?>">
+                <i class="fa fa-external-link icon"></i>
+            </a>
+            
+        </div>
+
+    </article>
+
+</div>
