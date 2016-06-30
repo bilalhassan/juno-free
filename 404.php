5535c716492f2f92c696df0778079e3bd198564a
Zack Mitchell <zackcmitchell@gmail.com>
2016-06-30 10:00:19 -0400

    Styled the 404 template page and related elements

--- 404.php
+++ 404.php
@@ -9,20 +9,35 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
 
+        <main id="main" class="site-main" role="main">
+        
+            <div id="single-page-container" class="container">
+
+                <div class="row">
+
+                    <div id="error-page-wrapper" class="col-md-12">
+                    
 			<section class="error-404 not-found">
+                            
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'juno' ); ?></h1>
-				</header><!-- .page-header -->
+					
+                                        <h1 class="page-title">
+                                            <?php echo esc_html( get_theme_mod( 'juno_error_page_heading', __( 'Oops!', 'juno' ) ) ); ?>
+                                        </h1>
+                                        
+                                        <p class="page-subtitle">
+                                            <?php echo esc_html( get_theme_mod( 'juno_error_page_subheading', __( 'It looks like nothing was found at this location, please check the address and try again!', 'juno' ) ) ); ?>
+                                        </p>
+                                        
+                                        <?php get_search_form(); ?>
+                                        
+                                </header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'juno' ); ?></p>
-
+					
 					<?php
-						get_search_form();
-
 						the_widget( 'WP_Widget_Recent_Posts' );
 
 						// Only show the widget if site has multiple categories.
@@ -57,8 +72,14 @@ get_header(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                        </div>
+                    </div>
+                
+                </div>
+                        
+        </main><!-- #main -->
+        
+    </div><!-- #primary -->
 
 <?php
 get_footer();
